fcce72b363708a9a0492d1c00057d9e6f4f537f8
Alonalos <alonalos8@gmail.com>
2024-08-30 01:46:41 +0000

    class9

---
+++ remove.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf hello bash
+sudo userdel -r tim
+sudo yum remove httpd -y
---
+++ worksheet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo Hello my name is Alona 
+echo Alona is currently studying Linux 
+echo Alona loves DevOps 
+echo $((26 * 30 * 12))  
